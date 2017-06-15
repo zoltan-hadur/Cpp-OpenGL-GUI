9e378d738feb4de6411a3cf51bd7be437e61db45
Hadur Zoltán <zotya701@hotmail.hu>
2017-06-15 22:32:37 +0200

    Check box now based on a button

--- GLUI/GLUI/CheckBox.h
+++ GLUI/GLUI/CheckBox.h
@@ -5,66 +5,86 @@
 namespace GLUI {
 
 	// A checkbox that can raise events when checked/unchecked
-	class CheckBox : public Button {
+	class CheckBox : public EventListener, public Component {
 	protected:
-		bool checked;	// Becomes true when the user clicks on a checkbox, and becomes false when the user clicks on the same checkbox again
+		Button* btn_check;								// The checkbox
+		bool checked;									// Becomes true when the user clicks on a checkbox, and becomes false when the user clicks on the same checkbox again
 
+		virtual void check_checkbox(Event& e);			// Raises a checkbox checked event
+		virtual void uncheck_checkbox(Event& e);		// Raises a checkbox unchecked event
 		virtual void handle_event(Event& e) override;
 		virtual void draw() override;
 	public:
+		// To listen on inner component events
+		virtual void action_performed(void* sender, Event& e) override;
 		// The coordinates of the checkbox, the size of the checkbox, and the border's width
 		CheckBox(float x = 0, float y = 0, float width = 20, float height = 20, float border_width = 1);
+		// Sets the checked state
+		void set_checked(bool checked);
 		// Returns true when checked, and false when not
-		bool get_checked();
+		bool is_checked();
 	};
 
+	// Raises a checkbox checked event
+	void CheckBox::check_checkbox(Event& e) {
+		this->checked = true;			// Check if it was unchecked, uncheck if it was checked
+		e.checkbox_checked = true;
+		e.checkbox_unchecked = false;
+		this->raise_event(this, e);		// Raise an event that the checkbox was checked/unchecked
+		e.checkbox_checked = false;
+		e.checkbox_unchecked = false;
+	}
+
+	// Raises a checkbox unchecked event
+	void CheckBox::uncheck_checkbox(Event& e) {
+		this->checked = false;			// Check if it was unchecked, uncheck if it was checked
+		e.checkbox_checked = false;
+		e.checkbox_unchecked = true;
+		this->raise_event(this, e);		// Raise an event that the checkbox was checked/unchecked
+		e.checkbox_checked = false;
+		e.checkbox_unchecked = false;
+	}
+
 	void CheckBox::handle_event(Event& e) {
-		float2 pos = this->get_absolute_position();											// The absolute position relative to the top-level window
-		if (pos.x < e.x && e.x < pos.x + width && pos.y < e.y && e.y < pos.y + height) {	// Check if the mouse was was above the button when the event happened
-			if (visible) {																	// Check if visible, because one does not simply press a button if it's invisible
-				this->highlighted = true;													// Highlight the button if the mouse is above the button
-				if (e.mouse_left && e.mouse_pressed) {										// Check if the user clicked on the button with the left mouse button
-					border_width = active_border_width;										// The checkbox press 'animation', simply increases the border until the event last
-					checked = !checked;														// Check if it was unchecked, uncheck if it was checked
-					e.checkbox_checked = checked;
-					e.checkbox_unchecked = !checked;
-					this->raise_event(this, e);												// Raise an event that the checkbox was checked/unchecked
-					e.checkbox_checked = false;
-					e.checkbox_unchecked = false;
-				}
-			}
-		} else {
-			this->highlighted = false;														// Disable the highligh when the mouse is not above the checkbox
-		}
-		if (e.mouse_left && e.mouse_released) {												// Check if the user released the left mouse button
-			border_width = default_border_width;											// The checkbox checked/unchecked 'animation' is over, decrease the border's width
-		}
+
 	}
 
 	void CheckBox::draw() {
-		Component::draw();																	// Draw the base component
+		if (checked) {
+			this->btn_check->get_label()->set_text("X");
+		} else {
+			this->btn_check->get_label()->set_text("");
+		}
+	}
 
-		float2 pos = this->get_absolute_position();											// Get the absolute position relative to the top-level window
-		if (checked) {																		// Draw a check mark if the checkbox is checked
-			glColor4f(1, 1, 1, 1);
-			glBegin(GL_LINES);
-			glVertex2f(pos.x + 2, pos.y + height / 2);
-			glVertex2f(pos.x + width / 2, pos.y - 2 + height);
-
-			glVertex2f(pos.x + width / 2, pos.y - 2 + height);
-			glVertex2f(pos.x - 2 + width, pos.y + 2);
-			glEnd();
+	// To listen on inner component events
+	void CheckBox::action_performed(void* sender, Event& e) {
+		if (sender == btn_check) {
+			if (e.button_pressed) {
+				if (!checked) {
+					this->check_checkbox(e);
+				} else {
+					this->uncheck_checkbox(e);
+				}
+			}
 		}
 	}
 
 	// The coordinates of the check box, the size of the check box, and the border's width
-	CheckBox::CheckBox(float x, float y, float width, float height, float border_width) : Button("", x, y, width, height, border_width) {
-		this->background_color = Color(150, 150, 150, 255);
-		checked = false;
+	CheckBox::CheckBox(float x, float y, float width, float height, float border_width) : Component(x, y, width, height, border_width) {
+		this->checked = false;
+		this->btn_check = new Button("", 0, 0, width, height, border_width);
+		this->btn_check->add_event_listener(this);
+		this->add_component(this->btn_check);
 	}
 
-	// Return true when checked, and false when not
-	bool CheckBox::get_checked() {
+	// Sets the checked state
+	void CheckBox::set_checked(bool checked) {
+		this->checked = checked;
+	}
+
+	// Returns true when checked, and false when not
+	bool CheckBox::is_checked() {
 		return checked;
 	}
 
