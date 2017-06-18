70581ca50696b280155eee7eeb5ea01b4362508f
Hadur Zoltán <zotya701@hotmail.hu>
2017-06-18 05:42:42 +0200

    Added project files

--- GLUI/main.cpp
+++ GLUI/main.cpp
@@ -9,27 +9,43 @@ const int screen_width = 192 * 5;			// Width of the window
 const int screen_height = 108 * 5;			// Height of the window
 int keys_down[256];							// For easy check of pressed keys
 float max_fps;								// Maximum allowed fps of the application
-GLUI::Window* window;
+GLUI::MainWindow* window;
 Event event;
 
-class fos : public EventListener {
+class ASD : public EventListener {
+private:
+	GLUI::Button* btn_start_progress;
+	GLUI::Button* btn_progress;
+	GLUI::Button* btn_stop_progress;
+	GLUI::ProgressBar* bar;
+	int value;
 public:
-	fos() {
+	ASD(GLUI::Button* start, GLUI::Button* progress, GLUI::Button* stop, GLUI::ProgressBar* bar) {
+		this->value = 0;
 
+		this->btn_start_progress = start;
+		this->btn_progress = progress;
+		this->btn_stop_progress = stop;
+		this->bar = bar;
+		this->bar->set_min(0);
+		this->bar->set_max(13);
 	}
-	int szar = 0;
 	void action_performed(void* sender, Event& e) override {
-		//GLUI::Button* btn = (GLUI::Button*)sender;
-		szar++;
-		std::cout << szar << std::endl;
-		//btn->get_label()->set_text(std::to_string(szar));
-		//if (e.mouse_scroll_up && e.mouse_pressed) {
-		//	szar++;
-		//} else if (e.mouse_scroll_down && e.mouse_pressed) {
-		//	szar--;
-		//}
-		//std::cout << szar << std::endl;
-		//btn->get_label()->set_text(std::to_string(szar));
+		if (e.button_pressed) {
+			if (sender == btn_start_progress) {
+				bar->start_progress();
+				this->value = 0;
+			}
+			if (sender == btn_progress) {
+				bar->set_value(++value);
+				if (value == 13) {
+					bar->stop_progress();
+				}
+			}
+			if (sender == btn_stop_progress) {
+				bar->stop_progress();
+			}
+		}
 	}
 };
 
@@ -38,17 +54,13 @@ void onInitialization() {
 
 	max_fps = 0;
 
-	GLUI::Panel* panel = new GLUI::Panel("Basic panel", 10, 10, 800, 500);
-	window->add_component(panel);
+	GLUI::Window* window_0 = new GLUI::Window("Window 0", 10, 10, 800, 500);
+	GLUI::Window* window_1 = new GLUI::Window("Window 1", 500, 10, 800, 500);
+	window->add_component(window_0);
+	//window->add_component(window_1);
 
-	GLUI::TabbedPanel* panel2 = new GLUI::TabbedPanel(20, 40, 600, 400);
-	panel2->add_tab("Tab names");
-	panel2->add_tab("has");
-	panel2->add_tab("automatic length");
-	GLUI::Panel* tab0 = panel2->get_tab("Tab names");
-	GLUI::Panel* tab1 = panel2->get_tab("has");
-	GLUI::Panel* tab2 = panel2->get_tab("automatic length");
-	panel->add_component(panel2);
+	GLUI::TabbedPanel* tabbed_panel = new GLUI::TabbedPanel(20, 40, 600, 400);
+	window_0->add_component(tabbed_panel);
 
 	GLUI::ScrollPanel* panel0 = new GLUI::ScrollPanel(GLUI::ScrollPanel::ALIGN::VERTICAL, 10, 10, 500, 350);
 	for (int i = 0; i < 6; ++i) {
@@ -56,7 +68,7 @@ void onInitialization() {
 			panel0->add_component(new GLUI::Button("B" + std::to_string(j * 6 + i), 10 + 75 * i, 10 + 25 * j, 70, 20));
 		}
 	}
-	tab0->add_component(panel0);
+	tabbed_panel->add_tab("Tab names", panel0);
 
 	GLUI::ScrollPanel* panel1 = new GLUI::ScrollPanel(GLUI::ScrollPanel::ALIGN::HORIZONTAL, 10, 10, 500, 350);
 	for (int i = 0; i < 12; ++i) {
@@ -64,8 +76,10 @@ void onInitialization() {
 			panel1->add_component(new GLUI::Button("B" + std::to_string(j * 12 + i), 10 + 75 * i, 10 + 25 * j, 70, 20));
 		}
 	}
-	tab1->add_component(panel1);
+	tabbed_panel->add_tab("has", panel1);
 
+	tabbed_panel->add_tab("automatic length");
+	GLUI::Panel* tab2 = tabbed_panel->get_tab("automatic length");
 
 	GLUI::ComboBox* cbox = new GLUI::ComboBox(10, 10, 200, 20);
 	cbox->add_element("1");
@@ -80,20 +94,35 @@ void onInitialization() {
 	cbox->add_element("10");
 	tab2->add_component(cbox);
 
-	GLUI::Button* btn0 = new GLUI::Button("ASD", 220, 10, 80, 20);
-	fos* szar = new fos();
-	btn0->add_event_listener(szar);
-	tab2->add_component(btn0);
+	//GLUI::Label* label = new GLUI::Label("CheckBox", 230, 10, 20, 20);
+	//tab2->add_component(label);
 
-	GLUI::CheckBox* check_box = new GLUI::CheckBox(310, 10, 20, 20);
-	tab2->add_component(check_box);
+	//GLUI::CheckBox* check_box = new GLUI::CheckBox(310, 10, 20, 20);
+	//tab2->add_component(check_box);
 
-	GLUI::TextBox* text_box0 = new GLUI::TextBox(340, 10, 250, 100);
-	text_box0->add_event_listener(szar);
-	tab2->add_component(text_box0);
+	//GLUI::TextBox* text_box0 = new GLUI::TextBox(GLUI::TextBox::TYPE::MULTI_LINE, 340, 10, 250, 100);
+	//tab2->add_component(text_box0);
 
-	GLUI::TextBox* text_box1 = new GLUI::TextBox(340, 120, 250, 20);
-	tab2->add_component(text_box1);
+	//GLUI::TextBox* text_box1 = new GLUI::TextBox(GLUI::TextBox::TYPE::SINGLE_LINE, 340, 120, 250, 20);
+	//tab2->add_component(text_box1);
+
+	//GLUI::ProgressBar* bar = new GLUI::ProgressBar(0, 100, 10, 230, 500, 20, 1);
+	//tab2->add_component(bar);
+
+	//GLUI::Button* btn_start_progress = new GLUI::Button("Start progress", 10, 260, 150, 20);
+	//GLUI::Button* btn_progress = new GLUI::Button("Progress", 185, 260, 150, 20);
+	//GLUI::Button* btn_stop_progress = new GLUI::Button("Stop progress", 360, 260, 150, 20);
+	//tab2->add_component(btn_start_progress);
+	//tab2->add_component(btn_progress);
+	//tab2->add_component(btn_stop_progress);
+
+	//ASD* asd = new ASD(btn_start_progress, btn_progress, btn_stop_progress, bar);
+	//btn_start_progress->add_event_listener(asd);
+	//btn_progress->add_event_listener(asd);
+	//btn_stop_progress->add_event_listener(asd);
+
+	GLUI::SpinnerBox<int>* spb_spinner = new GLUI::SpinnerBox<int>(0, 255, 10, 290, 200, 20);
+	tab2->add_component(spb_spinner);
 
 	watch.start();
 }
@@ -103,17 +132,40 @@ void onDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glBegin(GL_QUADS);
-	glColor3f(1, 0, 0);
-	glVertex2f(-1, -1);
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			float x = -1.0f + 2.0f / 10.0f * i;
+			float y = -1.0f + 2.0f / 10.0f * j;
+			float w = 2.0f / 10.0f;
+			float h = 2.0f / 10.0f;
 
-	glColor3f(1, 0, 0);
-	glVertex2f(-1, 1);
+			if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+				glColor3f(1, 0, 0);
+				glVertex2f(x, y);
 
-	glColor3f(0, 1, 0);
-	glVertex2f(1, 1);
+				glColor3f(0, 1, 0);
+				glVertex2f(x, y + h);
 
-	glColor3f(0, 1, 0);
-	glVertex2f(1, -1);
+				glColor3f(1, 0, 0);
+				glVertex2f(x + w, y + h);
+
+				glColor3f(0, 1, 0);
+				glVertex2f(x + w, y);
+			} else {
+				glColor3f(0, 1, 0);
+				glVertex2f(x, y);
+
+				glColor3f(1, 0, 0);
+				glVertex2f(x, y + h);
+
+				glColor3f(0, 1, 0);
+				glVertex2f(x + w, y + h);
+
+				glColor3f(1, 0, 0);
+				glVertex2f(x + w, y);
+			}
+		}
+	}
 	glEnd();
 
 	window->render();
@@ -134,8 +186,6 @@ void onKeyboard(unsigned char key, int x, int y) {
 	event.special_released = false;
 	event.x = x;
 	event.y = y;
-	event.dx = 0;
-	event.dy = 0;
 	window->event_handler(event);
 	event.key_pressed = false;
 	event.key_released = false;
@@ -156,8 +206,6 @@ void onKeyboardUp(unsigned char key, int x, int y) {
 	event.special_released = false;
 	event.x = x;
 	event.y = y;
-	event.dx = 0;
-	event.dy = 0;
 	window->event_handler(event);
 	event.key_pressed = false;
 	event.key_released = false;
@@ -176,8 +224,6 @@ void onSpecial(int key, int x, int y) {
 	event.special_released = false;
 	event.x = x;
 	event.y = y;
-	event.dx = 0;
-	event.dy = 0;
 	window->event_handler(event);
 	event.key_pressed = false;
 	event.key_released = false;
@@ -196,8 +242,6 @@ void onSpecialUp(int key, int x, int y) {
 	event.special_released = true;
 	event.x = x;
 	event.y = y;
-	event.dx = 0;
-	event.dy = 0;
 	window->event_handler(event);
 	event.key_pressed = false;
 	event.key_released = false;
@@ -224,8 +268,6 @@ void onMouse(int button, int state, int x, int y) {
 	event.mouse_right_down = (state == GLUT_DOWN && button == GLUT_RIGHT_BUTTON);
 	event.x = x;
 	event.y = y;
-	event.dx = 0;
-	event.dy = 0;
 	window->event_handler(event);
 	event.mouse_pressed = false;
 	event.mouse_released = false;
@@ -246,6 +288,8 @@ void onMouseMotion(int x, int y) {
 	event.dx = dx;
 	event.dy = dy;
 	window->event_handler(event);
+	event.dx = 0;
+	event.dy = 0;
 	event.mouse_moved = false;
 }
 
@@ -276,7 +320,7 @@ int main(int argc, char **argv) {
 	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
 	glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
 
-	window = new GLUI::Window("GLUI", 100, 100, screen_width, screen_height);
+	window = new GLUI::MainWindow("MainWindow", 100, 100, screen_width, screen_height);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
