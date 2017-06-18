b067cfcc2d2b9ac8ac7c74bbe818690cc00be76c
Hadur Zoltán <zotya701@hotmail.hu>
2017-06-18 05:31:53 +0200

    New panel

--- GLUI/GLUI/Panel.h
+++ GLUI/GLUI/Panel.h
@@ -4,69 +4,66 @@
 
 namespace GLUI {
 
-	// Panel, that can store and display various components, also can be moved with the mouse if the panel has a title
-	class Panel : public EventListener, public Component {
-	private:
-		Button* btn_title;	// Title of the panel
-		bool dragged;		// Becomes true when the user clicks on the title button, and false when releases the left mouse button
+	// Panel, that can store and display various components
+	class Panel : public Component {
 	protected:
+		bool draw_background;
+
 		virtual void handle_event(Event& e) override;
 		virtual void draw(bool draw_background = true) override;
+		virtual void render() override;
 	public:
-		// To listen on inner component events
-		virtual void action_performed(void* sender, Event& e) override;
-		// Title, the coordinates, the size, and the border's width
-		Panel(std::string title = "", float x = 0, float y = 0, float width = 100, float height = 100, float border_width = 2);
-		// Sets the title of the panel
-		void set_title(std::string title);
-		// Gets the title of the panel
-		std::string get_title();
+		// Draw background or not, the coordinates, the size, and the border's width
+		Panel(bool draw_background = true, float x = 0, float y = 0, float width = 100, float height = 100, float border_width = 2);
+
+		void set_draw_background(bool draw_background);
+		bool is_draw_background();
 	};
 
 	void Panel::handle_event(Event& e) {
-		if (this->dragged) {														// If dragged
-			if (e.mouse_moved) {													// And the mouse moved
-				this->set_position(this->get_position() + float2(e.dx, e.dy));		// Reposition the panel
-			}
-		}
+		
 	}
 
 	void Panel::draw(bool draw_background) {
-		std::string title = this->btn_title->get_label()->get_text();				// Get the title
-		if (title == "" || title == " ") {											// Don't draw it if it's empty
-			this->btn_title->set_visible(false);
-		}
 		Component::draw(draw_background);
 	}
 
-	// To listen on inner component events
-	void Panel::action_performed(void* sender, Event& e) {
-		if (sender == this->btn_title) {		// If the title
-			if (e.button_pressed) {				// Was pressed
-				this->dragged = true;			// Then the user probably going to drag the panel to somewhere else
-			} else if (e.button_released) {		// The user finished
-				this->dragged = false;			// Dragging the window
+	void Panel::render() {
+		if (this->visible) {
+			glMatrixMode(GL_PROJECTION); glPushMatrix(); glLoadIdentity();								// Save current projection matrix
+			glOrtho(0.0f, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT), 0.0f, -1.0f, 1.0f);	// Transform it to able to draw in pixel coordinates
+			glMatrixMode(GL_MODELVIEW); glPushMatrix(); glLoadIdentity();								// Save current modelview matrix
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); glEnable(GL_BLEND);						// Enable transparency
+
+			this->draw(this->draw_background);
+
+			float2 pos = this->get_absolute_position();
+			pos.x = pos.x + this->default_border_width;
+			pos.y = glutGet(GLUT_WINDOW_HEIGHT) - (pos.y + this->height - this->default_border_width);								// y is inverted
+			glScissor(pos.x, pos.y, this->width - this->default_border_width * 2, this->height - this->default_border_width * 2);	// Allows partially drawing components
+			glEnable(GL_SCISSOR_TEST);
+			auto children = this->children;
+			for (auto c : children) {
+				c->render();
 			}
+			glDisable(GL_SCISSOR_TEST);
+			glDisable(GL_BLEND);
+			glPopMatrix(); glMatrixMode(GL_PROJECTION);
+			glPopMatrix(); glMatrixMode(GL_MODELVIEW);
 		}
 	}
 
-	// Title, what is displayed in the top left corner, the coordinates, the size, and the border's width
-	Panel::Panel(std::string title, float x, float y, float width, float height, float border_width) : Component(x, y, width, height, border_width) {
-		this->dragged = false;
-		this->btn_title = new Button(" "+title, 0, 0, width, 20, border_width);
-		this->btn_title->get_label()->set_h_align(Label::H_ALIGN::LEFT);
-		this->btn_title->add_event_listener(this);
-		this->add_component(this->btn_title);
+	// Draw background or not, the coordinates, the size, and the border's width
+	Panel::Panel(bool draw_background, float x, float y, float width, float height, float border_width) : Component(x, y, width, height, border_width) {
+		this->draw_background = draw_background;
 	}
 
-	// Sets the title of the panel
-	void Panel::set_title(std::string title) {
-		this->btn_title->get_label()->set_text(title);
+	void Panel::set_draw_background(bool draw_background) {
+		this->draw_background = draw_background;
 	}
 
-	// Gets the title of the panel
-	std::string Panel::get_title() {
-		return this->btn_title->get_label()->get_text();
+	bool Panel::is_draw_background() {
+		return this->draw_background;
 	}
 
 }
