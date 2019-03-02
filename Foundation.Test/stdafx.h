7a4fbaabf424ef34f79d013db6819d933fc2cb6d
zotya701 <zotya701@gmail.com>
2019-03-02 13:19:57 +0100

    Incldue CRTDBG

--- Foundation.Test/stdafx.h
+++ Foundation.Test/stdafx.h
@@ -11,3 +11,12 @@
 #include "CppUnitTest.h"
 
 // TODO: reference additional headers your program requires here
+#define _CRTDBG_MAP_ALLOC  
+#include <stdlib.h>  
+#include <crtdbg.h>  
+
+#ifdef _DEBUG
+#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
+#else
+#define new new
+#endif
