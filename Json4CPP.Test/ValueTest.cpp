1c4c4175e564a4d717cfc972f7794796062eb517
zotya701 <zotya701@gmail.com>
2019-06-30 02:06:09 -0400

    Single test for ParseNull

--- Json4CPP.Test/ValueTest.cpp
+++ Json4CPP.Test/ValueTest.cpp
@@ -44,24 +44,7 @@ namespace Json4CPP::Test
       Assert::IsTrue(From(expected).SequenceEqual(output));
     }
 
-    TEST_METHOD(TestParseNull1)
-    {
-      auto input           = vector<wstring>{  L"", L" ", L" n", L" nu", L" nul", L" null", L" null ", L" nu11 ", L" nill ", L" mull " };
-      auto expectException = vector<bool>   { true, true,  true,   true,    true,    false,     false,      true,      true,      true };
-      for (int i = 0; i < input.size(); ++i)
-      {
-        if (!expectException[i])
-        {
-          Assert::IsTrue(nullptr == ParseNull(wstringstream(input[i])));
-        }
-        else
-        {
-          Assert::ExpectException<exception>([&]() { ParseNull(wstringstream(input[i])); });
-        }
-      }
-    }
-
-    TEST_METHOD(TestParseNull2)
+    TEST_METHOD(TestParseNull)
     {
       auto input           = vector<wstring>{  L"", L" ", L" n", L" nu", L" nul", L" null", L" null ", L" nu11 ", L" nill ", L" mull " };
       auto expectException = vector<bool>   { true, true,  true,   true,    true,    false,     false,      true,      true,      true };
@@ -70,13 +53,16 @@ namespace Json4CPP::Test
         if (!expectException[i])
         {
           Assert::IsTrue(nullptr == ParseNull(input[i]));
+          Assert::IsTrue(nullptr == ParseNull(wstringstream(input[i])));
         }
         else
         {
           Assert::ExpectException<exception>([&]() { ParseNull(input[i]); });
+          Assert::ExpectException<exception>([&]() { ParseNull(wstringstream(input[i])); });
         }
       }
     }
+
 
     TEST_METHOD(TestParseBoolean1)
     {
