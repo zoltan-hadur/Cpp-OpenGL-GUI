199e83503ad94c18e548e15d0998d60f694f101c
zotya701 <zotya701@gmail.com>
2019-07-16 21:53:22 -0400

    Refactor ValueTest

--- Json4CPP.Test/ValueTest.cpp
+++ Json4CPP.Test/ValueTest.cpp
@@ -21,7 +21,7 @@ namespace Json4CPP::Test
   public:
     TEST_METHOD(TestGetType1)
     {
-      auto input = vector<VALUE>{ nullptr_t(), wstring(), bool(), double(), JsonObject(), JsonArray() };
+      auto input    = vector<VALUE>   {    nullptr_t(),        wstring(),            bool(),         double(),     JsonObject(),     JsonArray() };
       auto expected = vector<JsonType>{ JsonType::Null, JsonType::String, JsonType::Boolean, JsonType::Number, JsonType::Object, JsonType::Array };
       auto output = From(input).Select<JsonType>([](VALUE const& value) { return GetType(value); });
       Assert::IsTrue(From(expected).SequenceEqual(output));
@@ -31,14 +31,20 @@ namespace Json4CPP::Test
     {
       auto input = vector<VALUE_BUILDER>
       {
-        nullptr_t(), wstring(), bool(), double(), JsonObject(), JsonArray(), vector<JsonBuilder>(), vector<JsonBuilder>{ JsonBuilder(L"Key"), JsonBuilder(L"Value") },
-        vector<JsonBuilder>{ vector<JsonBuilder>{ JsonBuilder(L"Key0"), JsonBuilder(L"Value0") }, vector<JsonBuilder>{ JsonBuilder(L"Key1"), JsonBuilder(L"Value1") } },
+        nullptr_t(), wstring(), bool(), double(), JsonObject(), JsonArray(),
+        vector<JsonBuilder>(),
+        vector<JsonBuilder>{ JsonBuilder(L"Key"), JsonBuilder(L"Value") },
+        vector<JsonBuilder>{ vector<JsonBuilder>{ JsonBuilder(L"Key0"), JsonBuilder(L"Value0") },
+                             vector<JsonBuilder>{ JsonBuilder(L"Key1"), JsonBuilder(L"Value1") } },
         vector<JsonBuilder>{ JsonBuilder(1), JsonBuilder(2), JsonBuilder(3) }
       };
       auto expected = vector<JsonBuilderType>
       {
         JsonBuilderType::Null, JsonBuilderType::String, JsonBuilderType::Boolean, JsonBuilderType::Number, JsonBuilderType::Object, JsonBuilderType::Array,
-        JsonBuilderType::Empty, JsonBuilderType::Pair, JsonBuilderType::Object, JsonBuilderType::Array
+        JsonBuilderType::Empty,
+        JsonBuilderType::Pair,
+        JsonBuilderType::Object,
+        JsonBuilderType::Array
       };
       auto output = From(input).Select<JsonBuilderType>([](VALUE_BUILDER const& value) { return GetType(value); });
       Assert::IsTrue(From(expected).SequenceEqual(output));
@@ -110,8 +116,8 @@ namespace Json4CPP::Test
         }
         else
         {
-          Assert::ExpectException<exception>([&]() { ParseBoolean(wstringstream(input[i])); });
           Assert::ExpectException<exception>([&]() { ParseBoolean(input[i]); });
+          Assert::ExpectException<exception>([&]() { ParseBoolean(wstringstream(input[i])); });
         }
       }
     }
