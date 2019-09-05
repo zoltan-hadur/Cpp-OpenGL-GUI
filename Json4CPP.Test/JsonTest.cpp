cf7169e8171c3daaf52ee5bb462f5ea64c1609bd
zotya701 <zotya701@gmail.com>
2019-09-04 23:11:46 -0400

    Test Json Literal

--- Json4CPP.Test/JsonTest.cpp
+++ Json4CPP.Test/JsonTest.cpp
@@ -10,6 +10,14 @@ namespace Json4CPP::Test
   TEST_CLASS(JsonObjectTest)
   {
   public:
-    
+    TEST_METHOD(TestLiteral)
+    {
+      Assert::AreEqual<Json>(nullptr, L"null"_json);
+      Assert::AreEqual<Json>(L"TestString", L"\"TestString\""_json);
+      Assert::AreEqual<Json>(true, L"true"_json);
+      Assert::AreEqual<Json>(13.37, L"13.37"_json);
+      Assert::AreEqual<Json>({ { L"Key1", 1 }, { L"Key2", 2 } }, L"{ \"Key1\": 1, \"Key2\": 2 }"_json);
+      Assert::AreEqual<Json>({ 1, 2, 3 }, L"[ 1, 2, 3 ]"_json);
+    }
   };
 }
