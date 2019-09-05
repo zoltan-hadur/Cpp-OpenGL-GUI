d3ba409268daa140b2a88baaf759876d34187c21
zotya701 <zotya701@gmail.com>
2019-09-04 23:01:46 -0400

    Add JsonTest.cpp

---
+++ Json4CPP.Test/JsonTest.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+using namespace Json4CPP;
+using namespace Json4CPP::Detail;
+
+namespace Json4CPP::Test
+{
+  TEST_CLASS(JsonObjectTest)
+  {
+  public:
+    
+  };
+}
