28a320273d74fcfe8a1b8396b7dae251fa4811d7
Zoltan Hadur <zotya701@hotmail.hu>
2022-05-16 15:43:39 +0200

    Move DeleteButton from view to control

--- Json4CPP.Visualizer/Control/DeleteButton.xaml.cs
+++ Json4CPP.Visualizer/Control/DeleteButton.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Json4CPP.Visualizer.View
+namespace Json4CPP.Visualizer.Control
 {
   /// <summary>
   /// Interaction logic for DeleteButton.xaml
